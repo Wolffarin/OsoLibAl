7467c6f8c70623d24bcc0684e6e5012e893a4a18
Wolffarin <Wolffarin1@gmail.com>
2020-03-19 17:09:23 -0600

    matriz 3x3

--- src/hn/edu/ujcv/progra/OMat3x3.java
+++ src/hn/edu/ujcv/progra/OMat3x3.java
@@ -10,15 +10,8 @@ public class OMat3x3 {
 
 
     public OMat3x3(double m11, double m12, double m13, double m21, double m22, double m23, double m31, double m32, double m33) {
-        this.m11 = m11;
-        this.m12 = m12;
-        this.m13 = m13;
-        this.m21 = m21;
-        this.m22 = m22;
-        this.m23 = m23;
-        this.m31 = m31;
-        this.m32 = m32;
-        this.m33 = m33;
+        this.m11 = m11; this.m12 = m12; this.m13 = m13; this.m21 = m21; this.m22 = m22;
+        this.m23 = m23; this.m31 = m31; this.m32 = m32; this.m33 = m33;
         double L = m11 + m12 + m13+ m21 + m22 + m23+  m31 + m32 + m33;
     }
 
