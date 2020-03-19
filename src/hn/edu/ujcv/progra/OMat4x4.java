e6511faa93fe0d41ce645f72b463ea5c1eab3f09
Wolffarin <Wolffarin1@gmail.com>
2020-03-19 17:10:48 -0600

    matriz 4x4

--- src/hn/edu/ujcv/progra/OMat4x4.java
+++ src/hn/edu/ujcv/progra/OMat4x4.java
@@ -12,22 +12,14 @@ public class OMat4x4 {
 
     public OMat4x4(double m11, double m12, double m13, double m14, double m21, double m22, double m23,double m24, double m31, double m32, double m33 , double m34,
                    double m41, double m42, double m43, double m44) {
-        this.m11 = m11;
-        this.m12 = m12;
-        this.m13 = m13;
-        this.m14 = m14;
-        this.m21 = m21;
-        this.m22 = m22;
-        this.m23 = m23;
-        this.m24 = m24;
-        this.m31 = m31;
-        this.m32 = m32;
-        this.m33 = m33;
-        this.m34 = m34;
-        this.m41 = m41;
-        this.m42 = m42;
-        this.m43 = m43;
-        this.m44 = m44;
+        this.m11 = m11;this.m12 = m12;
+        this.m13 = m13;this.m14 = m14;
+        this.m21 = m21;this.m22 = m22;
+        this.m23 = m23;this.m24 = m24;
+        this.m31 = m31;this.m32 = m32;
+        this.m33 = m33;this.m34 = m34;
+        this.m41 = m41;this.m42 = m42;
+        this.m43 = m43;this.m44 = m44;
 
         double L = m11 + m12 + m13 + m14+ m21 + m22 + m23 + m24 + m31 + m32 + m33 + m34 + m41 + m42 + m43 + m44;
     }
